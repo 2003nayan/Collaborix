889850bc9d6322e3741ba0a791130e736ad460ac
nayan-ai4m <nayan@ai4mtech.com>
2025-01-04 16:21:02 +0530

    Initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+
+function Header() {
+  const { user } = useUser();
+
+  return <div>{user && <h1>{user?.firstName}</h1>}</div>;
+}
+
+export default Header;
