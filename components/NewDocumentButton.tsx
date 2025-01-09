ab12bcd059089a2f60c788329bbdbb5f784539ab
nayan-ai4m <nayan@ai4mtech.com>
2025-01-09 17:49:03 +0530

    Fourth day from office

--- components/NewDocumentButton.tsx
+++ components/NewDocumentButton.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import { useTransition } from "react";
 import { Button } from "./ui/button";
 
 function NewDocumentButton() {
-  return <Button>New Document</Button>;
+  const [isPending, startTransition] = useTransition();
+
+  function handleCreateNewDocument() {
+    startTransition(async () => {
+      
+    });
+  }
+  return <Button onClick={handleCreateNewDocument}>New Document</Button>;
 }
 
 export default NewDocumentButton;
