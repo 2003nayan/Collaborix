15c509015cd1a9c2d2a30a29c3fd17c20e07da6b
nayan-ai4m <nayan@ai4mtech.com>
2025-01-27 13:03:43 +0530

    Changes - 7

--- components/Avatars.tsx
+++ components/Avatars.tsx
@@ -14,14 +14,16 @@ function Avatars() {
   const others = useOthers();
   const self = useSelf();
 
-  const all = [self, ...others];
+  const all = [self, ...others].filter(
+    (other): other is NonNullable<typeof other> => !!other?.id
+  );
 
   return (
     <div className="flex gap-2 items-center">
       <p className="font-light text-sm">Users currently editing this page</p>
       <div className="flex -space-x-5">
         {all.map((other, i) => (
-          <TooltipProvider key={other?.id + i}>
+          <TooltipProvider key={other.id + i}>
             <Tooltip>
               <TooltipTrigger>
                 <Avatar className="border-2 hover:z-50">
